2e1323c5b0c44c8092cf7bfc0e69eeb2741f4ecf
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-04-15 18:45:19 -0500

    ``IDE0090`` for ``CnfData`` in ``Game``

--- cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/Game.cs
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/Game.cs
@@ -5,7 +5,7 @@ namespace Rileysoft.DotHack.SLUS20267
     public static class Game
     {
         public const string ID = "SLUS-20267";
-        public static readonly CnfData CnfData = new CnfData
+        public static readonly CnfData CnfData = new()
         {
             BOOT2 = @"cdrom0:\SLUS_202.67;1",
             VER = "1.00",
