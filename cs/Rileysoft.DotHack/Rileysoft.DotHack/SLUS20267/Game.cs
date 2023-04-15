22476130e3dec551c6822504c8c35e8a900498a7
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-04-15 18:44:08 -0500

    ``CA1810`` for static initializer of ``Game``

--- cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/Game.cs
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/Game.cs
@@ -5,17 +5,15 @@ namespace Rileysoft.DotHack.SLUS20267
     public class Game
     {
         public const string ID = "SLUS-20267";
-        public static readonly CnfData CnfData;
+        public static readonly CnfData CnfData = new CnfData
+        {
+            BOOT2 = @"cdrom0:\SLUS_202.67;1",
+            VER = "1.00",
+            VMODE = "NTSC"
+        };
 
         static Game()
         {
-            CnfData = new CnfData
-            {
-                BOOT2 = @"cdrom0:\SLUS_202.67;1",
-                VER = "1.00",
-                VMODE = "NTSC"
-            };
-
             CnfData.MakeReadonly();
         }
     }
