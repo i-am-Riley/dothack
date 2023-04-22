8e89836164b09d0475d8a099b9697156efefcaa8
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-04-22 15:59:47 -0500

    C# - Add ``_f_ulltof`` method

--- cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/CodeWarrior/PS2Support/gcc_wrapper.cs
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/SLUS20267/CodeWarrior/PS2Support/gcc_wrapper.cs
@@ -8,7 +8,22 @@ namespace Rileysoft.DotHack.SLUS20267.CodeWarrior.PS2Support
     public static class gcc_wrapper
 
     {
-        public static void _dpflt() { } // left, right
+        /// <summary>
+        /// (_f_ulltof) - unsigned long long to float
+        /// </summary>
+        /// <param name="ul">unsigned long long</param>
+        /// <returns>float</returns>
+        public static float _ulltof (ulong ul)
+        {
+            return (float)ul;
+        }
+
+        /// <summary>
+        /// (_dpflt) - Double precision compare less than
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public static void _dpflt(double left, double right) { } // left, right
         public static void _dpfgt() { } // left, right
         public static void _dpfge() { } // left, right
 
