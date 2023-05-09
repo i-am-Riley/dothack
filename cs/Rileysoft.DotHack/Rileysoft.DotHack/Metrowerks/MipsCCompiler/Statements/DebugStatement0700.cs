deea5499abbdb9ab0f98a7ff1243544230ec4a45
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-05-09 16:59:39 -0500

    Add some DebugStatements

---
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/Metrowerks/MipsCCompiler/Statements/DebugStatement0700.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rileysoft.DotHack.Metrowerks.MipsCCompiler.Statements
+{
+    public class DebugStatement0700 : DebugStatement
+    {
+        public byte[] Bytes { get; set; }
+
+        public DebugStatement0700 (Stream stream) : base(0x07)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            Bytes = new byte[3];
+            stream.Read(Bytes, 0, 3);
+        }
+
+            
+    }
+}
