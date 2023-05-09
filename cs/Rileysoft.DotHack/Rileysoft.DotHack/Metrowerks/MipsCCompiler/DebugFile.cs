92b09057da86d0feead4fd200d9254358875109d
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-05-09 17:00:02 -0500

    Add DebugFile

---
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/Metrowerks/MipsCCompiler/DebugFile.cs
@@ -0,0 +1,22 @@
+﻿using Rileysoft.DotHack.Metrowerks.MipsCCompiler.Statements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rileysoft.DotHack.Metrowerks.MipsCCompiler
+{
+    public class DebugFile
+    {
+        public int Num1 { get; set; }
+        public int CompilerType { get; set; }
+
+        public List<DebugStatement> Statements { get; set; }
+
+        public DebugFile()
+        {
+            Statements = new List<DebugStatement>();
+        }
+    }
+}
