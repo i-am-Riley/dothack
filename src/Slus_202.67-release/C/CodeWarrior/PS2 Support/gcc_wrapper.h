c8dbfb690d83678879802fcc9e95481370e8f944
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-05-12 21:46:51 -0500

    Add gcc_wrapper.c
    Just playing around seeing if I can get a grasp on the source

---
+++ src/Slus_202.67-release/C/CodeWarrior/PS2 Support/gcc_wrapper.h
@@ -0,0 +1,7 @@
+
+float _f_ulltof(unsigned long long ul);
+int _dpfne(double left, double right);
+int _dpflt(double left, double right);
+int _dpfle(double left, double right);
+int _dpfgt(double left, double right);
+int _dpfge(double left, double right);
