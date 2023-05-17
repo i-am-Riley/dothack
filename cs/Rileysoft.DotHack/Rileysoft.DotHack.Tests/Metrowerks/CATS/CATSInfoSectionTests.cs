4fd6764e7d10b4b3547eec2c3e2a13bcae7cd7b1
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-05-17 10:44:21 -0500

    Metrowerks CATS: Add CATS Info Section

---
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack.Tests/Metrowerks/CATS/CATSInfoSectionTests.cs
@@ -0,0 +1,44 @@
+﻿using Rileysoft.DotHack.Metrowerks.CATS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rileysoft.DotHack.Tests.Metrowerks.CATS
+{
+    [TestClass]
+    public class CATSInfoSectionTests
+    {
+        [DataTestMethod]
+        [DataRow(
+            new byte[] { 0x02, 0x00, 0x3C, 0x00, 0xD0, 0x00, 0x10, 0x00 },
+            (byte)2, false, (ushort)0x3C, 0x001000d0, -1,
+            DisplayName = "Normal Exit")]
+        [DataRow(
+            new byte[] { 0x02, 0x01, 0x14, 0x00, 0x80, 0x0B, 0x10, 0x00, 0x0C, 0x00, 0x00, 0x00 },
+            (byte)2, true, (ushort)0x14, 0x00100b80, 0x0c,
+            DisplayName = "NSTD Exit")]
+        public void ReadFromStream_Inputs_ReturnsCorrectResult(
+            byte[] testData,
+            byte SectionType,
+            bool NSTDExit,
+            ushort Size,
+            int Address,
+            int Offset
+        )
+        {
+            CATSInfoSection catsInfo = new CATSInfoSection();
+            using (var ms = new MemoryStream(testData))
+            {
+                catsInfo.ReadFromStream(ms);
+            }
+
+            Assert.AreEqual(SectionType, catsInfo.SectionType);
+            Assert.AreEqual(NSTDExit, catsInfo.NSTDExit);
+            Assert.AreEqual(Size, catsInfo.Size);
+            Assert.AreEqual(Address, catsInfo.Address);
+            Assert.AreEqual(Offset, catsInfo.Offset);
+        }
+    }
+}
---
+++ cs/Rileysoft.DotHack/Rileysoft.DotHack/Metrowerks/CATS/CATSInfoSection.cs
@@ -0,0 +1,70 @@
+﻿using Rileysoft.Common.Extensions;
+
+namespace Rileysoft.DotHack.Metrowerks.CATS
+{
+    public class CATSInfoSection
+    {
+        /*
+			*** CATS INFO (.mwcats) ***
+
+00000000	Section type	2
+00000001		nstd-exit	0
+00000002		size		60
+00000004		address		001000d0
+...
+00000068	Section type	2
+00000069		nstd-exit	1
+0000006a		size		20
+0000006c		address		00100b80
+        		offset
+00000070		0000000c 
+        */
+
+        public byte SectionType { get; set; }
+        public bool NSTDExit { get; set; }
+        public ushort Size { get; set; }
+        public int Address { get; set; }
+
+        /// <summary>
+        /// Only read if NSTDExit is true.
+        /// </summary>
+        public int Offset { get; set; }
+
+        public CATSInfoSection()
+        {
+            SectionType = 0;
+            NSTDExit = false;
+            Size = 0;
+            Address = 0;
+            Offset = -1;
+        }
+
+        public CATSInfoSection(Stream stream) : this()
+        {
+            ReadFromStream(stream);
+        }
+
+        public void ReadFromStream (Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (!stream.CanRead)
+                throw new ArgumentException("stream cannot be read");
+
+            byte[] byteBuffer = new byte[1];
+
+            stream.Read(byteBuffer, 0, 1);
+            SectionType = byteBuffer[0];
+
+            stream.Read(byteBuffer, 0, 1);
+            NSTDExit = byteBuffer[0] != 0;
+
+            Size = stream.ReadUnsignedShortLE();
+            Address = stream.ReadIntLE();
+
+            if (NSTDExit)
+                Offset = stream.ReadIntLE();
+        }
+    }
+}
