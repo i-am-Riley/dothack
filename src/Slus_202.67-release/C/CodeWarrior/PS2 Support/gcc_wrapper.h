da951e8999d286496442ed2d1971f0a3400d88de
i-am-Riley <124444557+i-am-Riley@users.noreply.github.com>
2023-07-04 14:44:56 -0500

    Remove more stale code

--- src/Slus_202.67-release/C/CodeWarrior/PS2 Support/gcc_wrapper.h
+++
@@ -1,7 +0,0 @@
-
-float _f_ulltof(unsigned long long ul);
-int _dpfne(double left, double right);
-int _dpflt(double left, double right);
-int _dpfle(double left, double right);
-int _dpfgt(double left, double right);
-int _dpfge(double left, double right);
